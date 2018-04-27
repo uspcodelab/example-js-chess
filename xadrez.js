b16ca355272f48d2f19a35fd75c2e369de3e2ee3
João Daniel <jotaf.daniel@gmail.com>
2018-04-27 09:59:40 -0300

    Add variável

--- xadrez.js
+++ xadrez.js
@@ -11,12 +11,13 @@ function addWhite() {
 }
 
 function makeGrid() {
+    const gap = 50;
     for (let i = 0; i < 8; i++)
         for (let j = 0; j < 8; j++)
             if (i%2 == j%2)
-                setTimeout(addBlack, (i*8+j)*250);
+                setTimeout(addBlack, (i*8+j)*gap);
             else
-                setTimeout(addWhite, (i*8+j)*250);
+                setTimeout(addWhite, (i*8+j)*gap);
 }
 
 makeGrid()
